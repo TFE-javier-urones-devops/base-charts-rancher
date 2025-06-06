3cb2ee25b84e15cb590ebed84a3dad8bf0954634
jurones <javierum98@gmail.com>
2025-06-07 00:51:43 +0200

    Charts

--- modules/helm/rancher.tf
+++ modules/helm/rancher.tf
@@ -1,17 +1,17 @@
 
-resource "helm_release" "rancher" {
-  name       = "rancher"
-  chart      = "rancher"
-  repository = "https://releases.rancher.com/server-charts/stable"
+# resource "helm_release" "rancher" {
+#   name       = "rancher"
+#   chart      = "rancher"
+#   repository = "https://releases.rancher.com/server-charts/stable"
 
-  namespace        = "cattle-system"
-  create_namespace = true
+#   namespace        = "cattle-system"
+#   create_namespace = true
 
-  values = [
-    file("${path.module}/rancher/values-rancher.yaml")
-  ]
+#   values = [
+#     file("${path.module}/rancher/values-rancher.yaml")
+#   ]
 
-  depends_on = [
-    helm_release.ingress-nginx
-  ]
-}
+#   depends_on = [
+#     helm_release.ingress-nginx
+#   ]
+# }
