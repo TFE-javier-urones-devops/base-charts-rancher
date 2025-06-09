b534d4421f393951c4b9694abea7f05af904bd12
jurones <javierum98@gmail.com>
2025-06-09 20:43:44 +0200

    Refactorizando y organizando directorios

--- README.md
+++
@@ -1,3 +0,0 @@
-# Base-Charts-Rancher
-
-Este repositorio contiene los archivos de Terraform para desplegar los aplicativos (Charts) base para el clúster.
--- main.tf
+++
@@ -1,5 +0,0 @@
-module "helm" {
-  source = "./modules/helm"
-  certmanager_namespace = var.certmanager_namespace
-  argocd_namespace      = var.argocd_namespace
-}
